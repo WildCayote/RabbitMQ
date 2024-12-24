57675bf6ebc1add6ae89341c294bcd4529348dea
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-24 12:13:50 +0300

    Feat: Created a rabbit mq producer

--- test-app/src/app.service.ts
+++ test-app/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('TEST_SERVICE') private readonly client: ClientProxy) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async send_test(test: string) {
+    const response = await this.client.emit('test', test).toPromise();
+    return response;
   }
 }
